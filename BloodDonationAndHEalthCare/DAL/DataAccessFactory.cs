d6ddf45d2a5739fc87f85dc9fb59ad20644bd0c6
MdGolamRabbaniRafi <dreamlessboyrafi7@gmail.com>
2023-12-18 22:55:14 +0600

    User Update and Password Hash and Login

---
+++ BloodDonationAndHEalthCare/DAL/DataAccessFactory.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Interfaces;
+using DAL.Models;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        public static IRepo<UserAdmin, int, UserAdmin> UserAdminData()
+        {
+            return new UserAdminRepo();
+        }
+        public static IAuth<bool> AuthData()
+        {
+            return new UserAdminRepo();
+        }
+    }
+}
---
+++ BloodDonationAndHEalthCare/DAL/Migrations/202312181459284_createProfileOfAdmin.cs
@@ -0,0 +1,18 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class createProfileOfAdmin : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.UserAdmins", "BloodGroup", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.UserAdmins", "BloodGroup");
+        }
+    }
+}
