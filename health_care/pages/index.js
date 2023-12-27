229b66028e5500741c92a1d4aadfbc4255d224d7
fahad <evanahmedfahad@gmail.com>
2023-12-27 22:12:07 +0600

    pull

--- health_care/pages/index.js
+++ health_care/pages/index.js
@@ -3,14 +3,17 @@ import { Inter } from 'next/font/google'
 import Home1 from "../Components/Home"
 import Contact from '../Components/Contact'
 import About from '../Components/About'
+import AdminNav from './AdminNav'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
     <div>
       <Home1 />
+      <AdminNav />
       <Contact />
+
       <About />
     </div>
   )
